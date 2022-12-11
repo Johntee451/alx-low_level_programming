492dfd8118cae97b075af31b785228b229fa16ef
Johntee451 <johntee451@gmail.com>
2022-12-11 13:56:59 -0800

    diff c sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 
 		printf("Size of a char: %zu byte(s)\n", sizeof(char));
 
-			printf("Size of a int: %zu byte(s)\n", sizeof(int));
+			printf("Size of an int: %zu byte(s)\n", sizeof(int));
 
 				printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 
