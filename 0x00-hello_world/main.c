225ee18a0d4e5036b0af837ac869e6d67be51f42
Johntee451 <johntee451@gmail.com>
2022-12-11 07:29:46 -0800

    compling codes only

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ *main - Entry point
  *
  * Return: Always 0 (Success)
- */   
+*/
 
 int main(void)
 
 {
-	printf("hello world");
-	    return (0);
+	printf("hello world")
+	return (0);
 
 }
