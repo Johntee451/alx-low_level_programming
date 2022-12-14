e74a17897a5dc439a24d91e1bea374429244a47d
Johntee451 <johntee451@gmail.com>
2022-12-14 10:09:27 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int n;
+
+	for (n = 0; n < 10; ++n)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+	return (0);
+}
