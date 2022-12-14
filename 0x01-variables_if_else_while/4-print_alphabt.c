0298c4dfab2c3854e55f9e086b0aa755a3dc82cc
Johntee451 <johntee451@gmail.com>
2022-12-14 09:24:06 -0800

    skipped alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; ++n)
+	{
+		if (n != 'q' && n != 'e')
+		{
+			putchar(n);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
