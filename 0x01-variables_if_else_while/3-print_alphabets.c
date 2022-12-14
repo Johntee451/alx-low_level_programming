1b298c3c8b12c35965cb650c35d281efe266b08a
Johntee451 <johntee451@gmail.com>
2022-12-14 08:02:41 -0800

    printing lowe and upper case alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; ++n)
+	{
+		putchar(n);
+	}
+
+	for (n = 'A'; n <= 'Z'; ++n)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+	return (0);
+}
