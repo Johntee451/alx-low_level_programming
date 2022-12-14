741e7883229791b0ed9381a3337de7afffe79288
Johntee451 <johntee451@gmail.com>
2022-12-14 07:39:15 -0800

    printing lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; ++n)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
