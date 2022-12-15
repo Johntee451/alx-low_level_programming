0b0058c0ce54e0f587b48007e97f71bc55fd564e
Johntee451 <johntee451@gmail.com>
2022-12-14 17:55:46 -0800

    printing numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+
+	for (n = 97; n <= 102; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+	return (0);
+}
