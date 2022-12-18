d94e1dda05ef9d66cec549ca43f42ea6464c8fcf
Johntee451 <johntee451@gmail.com>
2022-12-18 01:02:23 -0800

    print out putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar to the screen
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+each files do their different functions
