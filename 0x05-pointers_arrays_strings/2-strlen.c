98db031c8a6ff996348673dda6d7dda2bb05eb33
Johntee451 <johntee451@gmail.com>
2022-12-19 14:49:19 -0800

    pointer

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen - swap the value.
+ * @s: the s pointer char variable.
+ * Return: Always 1.
+ */
+
+int _strlen(char *s)
+{
+	putchar(strlen(s));
+	return strlen(s);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,8 +2,12 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <string.h>
 
 void reset_to_98(int *n);
+
 void swap_int(int *a, int *b);
+
+int _strlen(char *s);
 
 #endif /* MAIN_H */
