5c3bb4ff116767497c9dc4c21d04fff358176143
Johntee451 <johntee451@gmail.com>
2022-12-19 06:58:34 -0800

    commitment

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -9,15 +9,41 @@ int main(void)
 {
 	int r;
 
-	r = _islower('H');
-
-	putchar(r + '0');
-
 	r = _islower('o');
 
 	putchar(r + '0');
 
 	r = _islower(108);
+
+	putchar(r + '0');
+
+	r = _islower('c');
+
+	r = _islower('I');
+
+	putchar(r + '0');
+
+	r = _islower('s');
+
+	putchar(r + '0');
+
+	r = _islower(70);
+
+	putchar(r + '0');
+
+	r = _islower(20);
+
+	putchar(r + '0');
+
+	r = _islower('U');
+
+	putchar(r + '0');
+
+	r = _islower('n');
+
+	putchar(r + '0');
+
+	r = _islower('H');
 
 	putchar(r + '0');
 
