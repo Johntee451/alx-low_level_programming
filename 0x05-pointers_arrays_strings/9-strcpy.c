56f04db17fb984228ea7d985e7ea1fdb6dbd2294
Johntee451 <johntee451@gmail.com>
2022-12-20 06:20:03 -0800

    new commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy string to each other.
+ * @dest: the destination.
+ * @src: the source.
+ * Return: dest.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
