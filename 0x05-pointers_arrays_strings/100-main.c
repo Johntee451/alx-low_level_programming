08028a898f08ef019dd25d4028f543e270e7a5a7
Johntee451 <johntee451@gmail.com>
2022-12-20 07:22:54 -0800

    new commit

--- 0x05-pointers_arrays_strings/100-main.c
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -10,21 +10,31 @@ int main(void)
 {
 	int nb;
 
-	nb = _atoi("98");
+	nb = _atoi("4");
 	printf("%d\n", nb);
-	nb = _atoi("-402");
+	nb = _atoi("10");
 	printf("%d\n", nb);
-	nb = _atoi("          ------++++++-----+++++--98");
+	nb = _atoi("-3");
 	printf("%d\n", nb);
-	nb = _atoi("214748364");
+	nb = _atoi("99");
 	printf("%d\n", nb);
-	nb = _atoi("0");
+	nb = _atoi("-40");
 	printf("%d\n", nb);
-	nb = _atoi("Suite 402");
+	nb = _atoi(" ------++++++-----+++++--98");
+	printf("%d\n", nb);
+	nb = _atoi("Hello ----- world\n");
+	printf("%d\n", nb);
+	nb = _atoi("+++++ +-+ 2242454");
+	printf("%d\n", nb);
+	nb = _atoi("2147483647");
 	printf("%d\n", nb);
 	nb = _atoi(" + + - -98 Battery Street; San Francisco, CA 94111 - USA");
 	printf("%d\n", nb);
-	nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
+	nb = _atoi("---++++ -++ Sui - te - 402 #cisfun :)");
+	printf("%d\n", nb);
+	nb = _atoi("");
+	printf("%d\n", nb);
+	nb = _atoi("-2147483648");
 	printf("%d\n", nb);
 	return (0);
 }
