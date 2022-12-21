ad55a6289fbf6d02378c9036c9a62ff73d9e25c7
Johntee451 <johntee451@gmail.com>
2022-12-20 21:39:51 -0800

    output from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - output from 0 to 9.
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,5 +7,7 @@
 
 int mul(int a, int b);
 
+void print_numbers(void);
+
 
 #endif /* MAIN_H */
