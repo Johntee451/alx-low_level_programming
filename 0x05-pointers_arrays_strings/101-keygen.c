094f52df5b2ce6ec5dcf3be73b84f822f88273ff
Johntee451 <johntee451@gmail.com>
2022-12-20 20:22:13 -0800

    random numbers

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - check the code.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+
+	n = rand();
+	printf("Tada! Congrats\n");
+
+	printf("%d\n", n);
+
+	return (0);
+}
