b80df07b526152d9723fff4aee38708f81aa9e98
Johntee451 <johntee451@gmail.com>
2022-12-21 14:55:54 -0800

    string concat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcat - concatintes strings.
+ * @dest: parameter destinatiion.
+ * @src:parameter sourse.
+ * Return: strr.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *strr;
+
+	strr = strcat(dest, src);
+
+	return (strr);
+}
