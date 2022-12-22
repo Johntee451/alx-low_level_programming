69be31e677f1e95147ff672c580a3c818032fd0b
Johntee451 <johntee451@gmail.com>
2022-12-21 20:51:23 -0800

    all the  way

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare strings.
+ * @s1: first string.
+ * @s2: second string.
+ * Return: integer.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int c;
+
+	c = strcmp(s1, s2);
+
+	return (c);
+}
