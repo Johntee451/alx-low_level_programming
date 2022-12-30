452b49b5e3e4a375006ee9872e3e1fab06239a2d
Johntee451 <johntee451@gmail.com>
2022-12-30 04:48:49 -0800

    is lower?

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -13,3 +13,24 @@ int _islower(int c)
 	else
 		return (0);
 }
+
+
+/**
+ * main - check the code.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int r;
+
+	r = _islower('H');
+	putchar(r + '0');
+	r = _islower('o');
+	putchar(r + '0');
+	r = _islower(108);
+	putchar(r + '0');
+	putchar('\n');
+
+	return (0);
+}
