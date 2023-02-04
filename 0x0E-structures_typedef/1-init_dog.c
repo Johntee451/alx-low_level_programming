5c9ad60f4e0f536643bc32b6349065449100bc90
Johntee451 <johntee451@gmail.com>
2023-02-04 05:53:57 -0800

    structures

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize members of dog
+ * @d: pointer to struct dog to modify members of
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
